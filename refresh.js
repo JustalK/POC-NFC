3b81888bf9b96efc851014c751ee35a765d060a0
Justal Kevin <justal.kevin@gmail.com>
2023-11-19 13:12:55 +0100

    [FEATURE] Adding sha read write

--- refresh.js
+++ refresh.js
@@ -1,28 +1,56 @@
 const cp = require("child_process");
+const fs = require("fs");
 const decompress = require("decompress");
 
-const URL =
-  "https://github.com/JustalK/PROJECT-PRESENTATION/archive/refs/heads/master.zip";
-const URL_PROJECT =
-  "https://api.github.com/repos/justalk/PROJECT-WIPEOUT/commits/master";
+const PROJECT_NAME = "PROJECT-WIPEOUT";
 
-const download = async function (url) {
-  let command = `curl -L -O ${url}`;
-  cp.execSync(command);
+/**
+ * Download the zip file from the url
+ */
+const download = function () {
+  cp.execSync(
+    `curl -L -O https://github.com/JustalK/${PROJECT_NAME}/archive/refs/heads/master.zip`
+  );
 };
 
-const getInfo = async function () {
-  let command = `curl ${URL_PROJECT} -H "Accept: application/json"`;
-  return cp.execSync(command);
+/**
+ * Get the actual sha from the project on gihtub
+ * @returns {String} The actual sha
+ */
+const getSha = function () {
+  const data = cp
+    .execSync(
+      `curl https://api.github.com/repos/justalk/${PROJECT_NAME}/commits/master`
+    )
+    .toString();
+  const info = JSON.parse(data);
+  const { sha } = info;
+  return sha;
 };
 
-async function test() {
-  const result = await getInfo();
-  console.log(result);
-  /**
-  await download(URL);
-  await decompress("master.zip", "dist");
-  **/
+/**
+ * Get the previous sha from the file containing the latest version
+ * @returns {String} The latest sha
+ */
+const getPreviousSha = function () {
+  const previousResult = fs.readFileSync("./sha.json");
+  const previousInfo = JSON.parse(previousResult);
+  return previousInfo.sha;
+};
+
+/**
+ * Update the project if the sha version are different bewteen actual project and the version on the server
+ */
+async function update() {
+  const sha = getSha();
+  const previousSha = getPreviousSha();
+
+  if (sha !== previousSha) {
+    fs.writeFileSync("sha.json", JSON.stringify({ sha }));
+    download();
+    await decompress("master.zip", ".");
+    fs.unlinkSync("master.zip");
+  }
 }
 
-test();
+update();
